891f7c8480fd2782ddaaf69321afea71f6a17bfc
NatSim <natsimbig@gmail.com>
2020-09-13 23:33:21 +0100

    book shop looping through book array

--- index1.js
+++ index1.js
@@ -1,1 +1,2 @@
-//import section
+import { books } from "./bookshopindex.js";
+import createListOfBooks from "./listOfBooks";
--- listOfBooks.js
+++ listOfBooks.js
@@ -1,1 +1,10 @@
-//Function area
+export default function createListOfBooks(books) {
+    const bookListSection = document.querySelector("#list");
+    const title = document.createElement("h2");
+    const titleText = document.createTextNode("Suggested Reading");
+    const bookDiv = document.createElement("div");
+
+}
+
+               //Function area
+
