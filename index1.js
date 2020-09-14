5cd519eb0e0ad77c013b375b67c39329b2fee75e
NatSim <natsimbig@gmail.com>
2020-09-14 16:17:46 +0100

    Ready for Carlos to fix

--- index1.js
+++ index1.js
@@ -1,2 +1,169 @@
-import { books } from "./bookshopindex.js";
-import createListOfBooks from "./listOfBooks";
+import { books } from "/bookshopindex.js";
+import createListOfBooks from "./listOfBooks.js"
+  createListOfBooks(books);
+
+ const books =[{
+    name:"Off Camera",
+    author:"Marcia Layne", 
+    price:"£9.99",
+    description:"Two London girls visit Jamaica for a holiday and for romance. Babs wants to know if her romance with Passion, a guy from last year, is for keeps. Anisha is there for different reasons, but she isn't saying what they are - yet. Off Camera was performed at the West Yorkshire Playhouse, 27 June - 12 July, 2003, and was winner of the Alfred Fagon Prize.", 
+    image:"https://cdn.waterstones.com/bookjackets/large/9781/8400/9781840023817.jpg"
+   }, 
+    {
+    name:"The Beach",
+    author:"Alex Garland",
+    price:"£7.99",
+    description:"Richard lands in East Asia in search of an earthly utopia. In Thailand,he is given a map promising an unknown island, a secluded beach - and a new way of life. What Richard finds when he gets there is breathtaking: more extraordinary,more frightening than his wildest dreams.But how long can paradise survive here on Earth? And what lengths will Richard go to in order to save it?",
+    image:" https://images.gr-assets.com/books/1432241375l/2044367.jpg"
+   },
+    {
+    name:"Natives: Race and Class in the Ruins of Empire - The Sunday Times Bestseller", 
+    author:"Akala",
+    price:"£8.99", 
+    description:"A searing modern polemic and Sunday Times bestseller from the BAFTA and MOBO award-winning musician and political commentator, Akala. From the first time he was stopped and searched as a child,to the day he realised his mum was white, to his first encounters with racist teachers - race and class have shaped Akala's life and outlook.In this unique book he takes his own experiences and widens them out to look at the social,historical and political factors that have left us where we are today.Covering everything from the police, education and identity to politics, sexual objectification and the far right,Natives will speak directly to British denial and squeamishness when it comes to confronting issues of race and class that are at the heart of the legacy of Britain's racialised empire.",
+    image:"https://images-na.ssl-images-amazon.com/images/I/51ya6G0fWUL._SX324_BO1,204,203,200_.jpg"
+   },
+    {
+    name:"Queenie",
+    author:"Candice Carty-Williams",
+    price:"£8.99", 
+    description:"THE SUNDAY TIMES BESTSELLER SHORTLISTED FOR THE COSTA FIRST NOVEL AWARD LONGLISTED FOR THE WOMEN'S PRIZE FOR FICTION 'Hilarious, compelling, painful,enlightening, honest. I loved it' - Dolly Alderton 'Brilliant, timely, funny, heartbreaking' - Jojo Moyes 'A deliciously funny, characterful, topical ...</summary>",
+    image:"http://books.google.com/books/content?id=NhBmDwAAQBAJ&amp;printsec=frontcover&amp;img=1&amp;zoom=1&amp;edge=curl&amp;source=gbs_api"
+   },
+    {
+    name:"The Dark Lady" , 
+    author:"AKALA", 
+    price:"£12.99" , 
+    description:"Having found success and acclaim with searing adult non-fiction and children’s picture books, the multi-talented rapper, poet and author Akala turns his hand to YA with the first in a magical realist series set in Shakespearean London. Written with the kind of lyrical dexterity and power that one would expect from Akala, The Dark Lady references the Bard’s sonnets as well as crafting an uncompromising picture of street life in Renaissance England.", 
+    image:"C:\Users\Natasha\Desktop\Learning\images\theDarkLady.jpg"
+   },
+    {
+    name:"The Girl Who Smiled Beads" ,
+    author:"Clemantine Wamariya" ,
+    price:"£3.99" , 
+    description:"When Clemantine Wamariya was six years old, her world was torn apart. She didn't know why her parents began talking in whispers, or why her neighbours started disappearing, or why she could hear distant thunder even when the skies were clear. Asthe Rwandan civil war raged, Clemantine and her sister...</summary>",
+    image:"http://books.google.com/books/content?id=djc_DwAAQBAJ&amp;printsec=frontcover&amp;img=1&amp;zoom=1&amp;edge=curl&amp;source=gbs_api"
+   },
+    {
+    name:"Americanah" , 
+    author:"Chimamanda Ngozi Adichie" ,
+    price: "£3.49" , 
+    description:"When Clemantine Wamariya was six years old, her world was torn apart. She didn't know why her parents began talking in whispers, or why her neighbours started disappearing, or why she could hear distant thunder even when the skies were clear. As the Rwandan civil war raged, Clemantine and her sister...</summary>",
+    image:"http://books.google.com/books/content?id=QXHwNEB2CkYC&amp;printsec=frontcover&amp;img=1&amp;zoom=1&amp;edge=curl&amp;source=gbs_api"
+   },
+    {
+    name:"What We Lose" , 
+    author:"Zinzi Clemmons" ,
+    price:"£2.99" , 
+    description:"A short, intense and profoundly moving novel about race,identity, family, sex, country, and death – from one of the National Book Foundation’s 5 Under 35: an author of rare, haunting power this is a stunning debut about a young African-American woman coming of age. Raised in Pennsylvania, Thandi views the world of her mother’s childhood in Johannesburg as both impossibly distant and ever present. She is an outsider wherever she goes, caught between being black and white...",
+    image:"http://books.google.com/books/content?id=NWrpDQAAQBAJ&amp;printsec=frontcover&amp;img=1&amp;zoom=1&amp;edge=curl&amp;source=gbs_api"
+   },
+    {
+    name:"The Good Lord Bird" , 
+    author:"James McBride" ,
+    price:"£8.38" , 
+    description:"Soon to be a Showtime limited series starring Ethan Hawke Winner of the National Book Award for Fiction A Washington Post, Publishers Weekly, Oprah Magazine Top 10 Book of the Year From the bestselling author of The Color of Water, Song Yet Sung, Five-Carat Soul, and Kill 'Em and Leave, a James Brow...</summary>",
+    image:"http://books.google.com/books/content?id=BnudfIm9NMUC&amp;printsec=frontcover&amp;img=1&amp;zoom=1&amp;edge=curl&amp;source=gbs_api"
+   },
+    {
+    name:"Ordinary Light" , 
+    author:"by John S. Green" ,
+    price:"£3.83" , 
+    description:"Real, raw, and funny, the voice of John S. Green takes you on a lifelong journey of seeking sacred resting places in the middle of lifes ever increasing chaos and distractions. Headlines scream, Beware! Look out! The end is near! A lightning, flashing, neon pulse that blinds us from the ordinary lig...",
+    image:"http://books.google.com/books/content?id=xeHGDgAAQBAJ&amp;printsec=frontcover&amp;img=1&amp;zoom=1&amp;edge=curl&amp;source=gbs_api"
+   },
+    {
+    name:"The Water Dancer" , 
+    author:"Ta-Nehisi Coates" ,
+    price:"£6.99" , 
+    description:"The unmissable debut novel by the critically acclaimed author of `Between the World and Me andWe Were Eight Years in Power` - a richly imagined and compulsively page-turning journey to freedom OPRAH BOOK CLUB PICK 'I haven't felt this way since I first read Beloved... I wish Toni [Morrison] was alive ...",
+    image:"http://books.google.com/books/content?id=f2KMDwAAQBAJ&amp;printsec=frontcover&amp;img=1&amp;zoom=1&amp;edge=curl&amp;source=gbs_api"
+   },
+   {
+    name:"Known and Strange Things" , 
+    author:"Teju Cole" ,
+    price:"£5.03" , 
+    description:"A blazingly intelligent first collection of essays from the award-winning author of Open City and Every Day Is for the Thief. With these pieces on politics, photography, travel, history and literature - many of which have become viral sensations, shared and debated around the globe - Teju Cole solid...",
+    image:"http://books.google.com/books/content?id=tiSPDAAAQBAJ&amp;printsec=frontcover&amp;img=1&amp;zoom=1&amp;edge=curl&amp;source=gbs_api"
+   },
+    {
+    name:"Black Is the Body" , 
+    author:"Black Is the Body" ,
+    price:"£3.63" , 
+    description:"The host of The Daily Show, Trevor Noah, tells the story of growing up mixed race in South Africa under and after apartheid in this young readers'adaptation of his bestselling adult memoir Born a Crime: Stories from a South African Childhood. BORN A CRIME IS SOON TO BE A MAJOR MOTION PICTURE STARRI...",
+    image:"http://books.google.com/books/content?id=OqJaDwAAQBAJ&amp;printsec=frontcover&amp;img=1&amp;zoom=1&amp;edge=curl&amp;source=gbs_api"
+   },
+    {
+    name:"It's Trevor Noah: Born a Crime" , 
+    author:"Trevor Noah" ,
+    price:"£3.99" , 
+    description:"The host of The Daily Show, Trevor Noah, tells the story of growing up mixed race in South Africa under and after apartheid in this young readers' adaptation of his bestselling adult memoir Born a Crime: Stories from a South African Childhood. BORN A CRIME IS SOON TO BE A MAJOR MOTION PICTURE STARRI...",
+    image:"http://books.google.com/books/content?id=Kv16DwAAQBAJ&amp;printsec=frontcover&amp;img=1&amp;zoom=1&amp;edge=curl&amp;source=gbs_api"
+   },
+    {
+    name:"Zone One" , 
+    author:"Colson Whitehead" ,
+    price:"£4.99" , 
+    description:"From the author of the Man Booker longlisted The Underground Railroad A pandemic has devastated the planet, sorting humanity into two types: the uninfected and the infected, the living and the living dead. The worst of the plague is now past,and Manhattan is slowly being resettled. Armed forces hav...",
+    image:"http://books.google.com/books/content?id=udzdZ7oHIdgC&amp;printsec=frontcover&amp;img=1&amp;zoom=1&amp;edge=curl&amp;source=gbs_api"
+   },
+    {
+    name:"We Cast a Shadow" , 
+    author:"Maurice Carlos Ruffin" ,
+    price:"£4.05" , 
+    description:"“An incisive and necessary” (Roxane Gay) debut for fans of Get Out and Paul Beatty’s The Sellout, about a father’s obsessive quest to protect his son—even if it means turning him white Longlisted for the Center for Fiction First Novel Prize • “Stunning and audacious . . . at once a pitch-black comed...",
+    image:"http://books.google.com/books/content?id=CZJbDwAAQBAJ&amp;printsec=frontcover&amp;img=1&amp;zoom=1&amp;edge=curl&amp;source=gbs_api"
+   },
+    {
+    name:"Black Leopard, Red Wolf" , 
+    author:"by Marlon James" ,
+    price:"£4.99" , 
+    description:"The Sunday Times Number 1 Bestseller and A New York Times Bestseller'Black Leopard, Red Wolf is the kind of novel I never realized I was missing until I read it. A dangerous, hallucinatory, ancient Africa, which becomes a fantasy world as well-realized as anything Tolkien made, with language as pow...",
+    image:"http://books.google.com/books/content?id=ExJdDwAAQBAJ&amp;printsec=frontcover&amp;img=1&amp;zoom=1&amp;edge=curl&amp;source=gbs_api"
+   },
+    {
+    name:"More Than Enough" , 
+    author:"Elaine Welteroth" ,
+    price:"£4.99" , 
+    description:"In this part-manifesto, part-memoir, the revolutionary editor who infused social consciousness into the pages of Teen Vogue explores what it means to come into your own – on your own terms. Elaine Welteroth has climbed the ranks of media and fashion, shattering ceilings along the way. In this riveti...",
+    image:"http://books.google.com/books/content?id=vE6MDwAAQBAJ&amp;printsec=frontcover&amp;img=1&amp;zoom=1&amp;edge=curl&amp;source=gbs_api"
+   },
+    {
+    name:"How Beautiful We Were" , 
+    author:"Imbolo Mbue" ,
+    price:"No Retail Price" , 
+    description:"'A formidable storyteller' JONATHAN FRANZEN Set in the fictional African village of Kosawa, How Beautiful We Were tells the story of a people living in fear amidst environmental degradation wrought by an American oil company. Pipeline spills have rendered farmlands infertile. Children are dying from...",
+    image:"http://books.google.com/books/content?id=GNjBDwAAQBAJ&amp;printsec=frontcover&amp;img=1&amp;zoom=1&amp;source=gbs_api"
+   },
+    {
+    name:"Behold the Dreamers" , 
+    author:"Imbolo Mbue" ,
+    price:"£3.99" , 
+    description:"A NEW YORK TIMES BOOK OF THE YEAR AN OBSERVER BOOK OF THE YEAR OPRAH BOOK CLUB PICK A powerful and timely story of marriage, class, race and the pursuit of the American Dream. Behold the Dreamers is a dazzling debut novel about life, liberty and the pursuit of happiness – and of what we’re prepared ...",
+    image:"http://books.google.com/books/content?id=R16QCgAAQBAJ&amp;printsec=frontcover&amp;img=1&amp;zoom=1&amp;edge=curl&amp;source=gbs_api"
+   },
+    {
+    name:"What Is Not Yours Is Not Yours" , 
+    author:"Helen Oyeyemi" ,
+    price:"£6.99" , 
+    description:"The stories collected in What Is Not Yours Is Not Yours are linked by more than the exquisitely winding prose of their creator: Helen Oyeyemi's ensemble cast of characters slip from the pages of their own stories only to surface in another. The reader is invited into a world of lost libraries and lo..." ,
+    image:"http://books.google.com/books/content?id=ELG6CgAAQBAJ&amp;printsec=frontcover&amp;img=1&amp;zoom=1&amp;edge=curl&amp;source=gbs_api"
+   },
+    {
+    name:"Homegoing" , 
+    author:"Yaa Gyasi" ,
+    price:"£3.99" , 
+    description:"THE SUNDAY TIMES BESTSELLER Selected for Granta's Best of Young American Novelists 2017 Winner of the National Book Critics Circle Award for Best First Book Shortlisted for the PEN/Robert W. Bingham Prize for Debut Fiction Effia and Esi:two sisters with two very different destinies. One sold into s...",
+    image:"http://books.google.com/books/content?id=L7c4CwAAQBAJ&amp;printsec=frontcover&amp;img=1&amp;zoom=1&amp;edge=curl&amp;source=gbs_api"
+   },
+    {
+    name:"Red at the Bone" ,
+    author:"Jacqueline Woodson" ,
+    price:"£7.99" , 
+    description:"THE TOP 10 NEW YORK TIMES BESTSELLER LONGLISTED FOR THE WOMEN'S PRIZE 2020 'Will remind you why you love reading' Stylist (Best New Books for 2020)'Sublime... will strike you in the heart' Candice Carty-Williams, author of Queenie'Profound, moving and consistently unexpected... Pure poetry' Observ...",
+    image:"http://books.google.com/books/content?id=GLisDwAAQBAJ&amp;printsec=frontcover&amp;img=1&amp;zoom=1&amp;edge=curl&amp;source=gbs_api"
+   },
+  
+   ];
+
+console.log(books.name);
--- listOfBooks.js
+++ listOfBooks.js
@@ -3,8 +3,49 @@ export default function createListOfBooks(books) {
     const title = document.createElement("h2");
     const titleText = document.createTextNode("Suggested Reading");
     const bookDiv = document.createElement("div");
+  //Function area MAP Method 1
 
-}
+  book.map((book) => {
+      const bookDivItem = document.createElement("div");
+      bookDivItem.className = "book"; 
+      const bookImage = document.createElement("img"); 
+      bookImage.src = book.image;
+      bookImage.alt = book.name;
+      const bookContentDiv = document.createElement("h2");
+      bookTitle.title = book.name;
+      const bookAuthor = document.createElement("p");
+      bookAuthor.src = book.author;
+      bookAuthor.textContent = book.author;
+      const bookPrice = document.createElement("p");
+      bookPrice.className = ("price");
+      bookPrice.textContent = book.price;
+      const bookDescription = document.createElement("summary");
+      bookDescription.className = ("description");
+      bookDescription.textContent = book.description;
+      const buttonDiv = document.createElement("div");
+      const addBookButton = document.createElement("button")
+      addBookButton.class = "addbtn";
+      addBookBtn.textContent = "Add +";
 
-               //Function area
+      bookContentDiv.appendChild(bookTitle);
+      bookContentDiv.appendChild(bookAuthor);
+      bookContentDiv.appendChild(bookPrice);
+      bookContentDiv.appendChild(bookDescription);
+  
+      bookDivItem.appendChild(bookImage);
+      bookDivItem.appendChild(bookContentDiv);
+      buttonDiv.appendChild(addBookBtn);
+      bookDivItem.appendChild(buttonDiv);
+      bookDiv.appendChild(bookDivItem);
+    });
+  
+    title.appendChild(titleText);
+    bookListSection.appendChild(title);
+    bookListSection.appendChild(bookDiv);
+
+
+
+  }
+
+ console.log(createListOfBooks);            
 
